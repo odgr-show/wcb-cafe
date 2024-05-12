758248b478245869d05e3c4a22f3f24333bb409d
odgr-show <121861788+odgr-show@users.noreply.github.com>
2024-05-13 00:43:01 +0900

    WCB Cafeのサイトの作成を始めました。 homeとnewsフォームが現在途中となってます。

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+const root = "src";
+
+export default defineConfig({
+    root,
+    base: "/",
+    publicDir: "../public",
+    plugins: [],
+    build: {
+        outDir: "../dist",
+        rollupOptions: {
+            input: {
+                // htmlを追加する場合にはこちらに追記
+                index: resolve(root, "index.html"),
+                news: resolve(root, "news.html"),
+            },
+        },
+    },
+    server: {
+        host: true,
+    },
+});
